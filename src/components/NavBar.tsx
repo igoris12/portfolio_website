8ecfb91488899f5145b74e0b390fd3c1675f1af4
Igoris Ivanovas <igoris.x@inbox.lt>
2022-01-03 10:21:25 +0200

    BavBar list style

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,14 @@ const NavBar = () => {
     return (
         <div className='navBar'>
             <a className='logo'><img src={logo} alt="logo" /></a>
-            <nav className='list'>Nav bar</nav>
+            <nav className='list'>
+                <ul>
+                    <li className='listItem'>About</li>
+                    <li className='listItem'>Projects</li>
+                    <li className='listItem'>Services</li>
+                    <li className='listItem'>Contact</li>
+
+                </ul></nav>
             <div className='hamburger'>hamburger</div>
         </div>
     )
