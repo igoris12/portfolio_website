50dc3adb2b6b379401bb4287487e1825341fe5ca
Igoris Ivanovas <igoris.x@inbox.lt>
2022-01-03 20:25:07 +0200

    toggle active mobile navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,25 +5,33 @@ import {AiOutlineGithub, AiFillLinkedin} from 'react-icons/ai'
 
 const NavBar = () => {
     const [mobileScreen , setMobileScreen] = useState<boolean>(false);
+    const [mobileActive , setMobileActive] = useState<boolean>(false);
+
     useEffect(() => {
         window.innerWidth <= 770 ? setMobileScreen(true) : setMobileScreen(false);
         return     window.addEventListener('resize', () => {
         window.innerWidth <= 770 ? setMobileScreen(true) : setMobileScreen(false);
         
     })
-    })
+    }, [])
     
 
+    const toggleMobileActive = () => {
+        setMobileActive(!mobileActive)
+    }
 
     
     return (
         <div className='navBar'>
             <a className='logo'><img src={logo} alt="logo" /></a>
-            <nav className='list active'>
+            <nav className={`list ${mobileActive ? 'active': null}`}>
                 <ul>
-                {mobileScreen && <a className='avatar listItem' ><div className='avatarContainer'>
-                    <img src={avatar} alt="Avatar" /></div>
-                <p >Igoris Ivanovas</p></a>}
+                {mobileScreen &&
+                 <a className='avatar listItem' >
+                     <div className='avatarContainer'>
+                        <img src={avatar} alt="Avatar" />
+                    </div>
+                <p>Igoris Ivanovas</p></a>}
                     <a className='listItem'>About</a>
                     <a className='listItem'>Projects</a>
                     <a className='listItem'>Services</a>
@@ -34,8 +42,8 @@ const NavBar = () => {
                     <a href="https://www.linkedin.com/in/igoris-ivanovas-839645194/" target="_blank" ><AiFillLinkedin/></a>
                 </div>}
             </nav>
-            <div className='hamburger'>
-                <div className='hmItem1'>
+            <div className={`hamburger ${mobileActive ? 'active': null}`} onClick={() => toggleMobileActive()}>
+                <div className='hmItem1' >
                     </div>< div className='hmItem2'></div><div className='hmItem3'></div>
                 </div>
         </div>
