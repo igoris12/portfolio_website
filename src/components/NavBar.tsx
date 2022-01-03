cdaf3669644b43535ab6cadd4bd664f943fcd994
Igoris Ivanovas <igoris.x@inbox.lt>
2022-01-03 20:09:46 +0200

    add socials links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import logo from "../img/logo.png";
 import avatar from "../img/avatar.png";
-
+import {AiOutlineGithub, AiFillLinkedin} from 'react-icons/ai' 
 
 const NavBar = () => {
     const [mobileScreen , setMobileScreen] = useState<boolean>(false);
@@ -21,16 +21,23 @@ const NavBar = () => {
             <a className='logo'><img src={logo} alt="logo" /></a>
             <nav className='list active'>
                 <ul>
-                {mobileScreen && <a className='avatar listItem' ><div className='avatarContainer'><img src={avatar} alt="Avatar" /></div>
+                {mobileScreen && <a className='avatar listItem' ><div className='avatarContainer'>
+                    <img src={avatar} alt="Avatar" /></div>
                 <p >Igoris Ivanovas</p></a>}
                     <a className='listItem'>About</a>
                     <a className='listItem'>Projects</a>
                     <a className='listItem'>Services</a>
                     <a className='listItem'>Contact</a>
                 </ul>
-                {/* icons  */}
+                {mobileScreen && <div className='socialsLinks' >
+                    <a href="https://github.com/igoris12" target="_blank" >< AiOutlineGithub /></a>
+                    <a href="https://www.linkedin.com/in/igoris-ivanovas-839645194/" target="_blank" ><AiFillLinkedin/></a>
+                </div>}
             </nav>
-            <div className='hamburger'><div className='hmItem1'></div>< div className='hmItem2'></div><div className='hmItem3'></div></div>
+            <div className='hamburger'>
+                <div className='hmItem1'>
+                    </div>< div className='hmItem2'></div><div className='hmItem3'></div>
+                </div>
         </div>
     )
 }
