f690dcfa83de7d3eaed109d01bef021d0e662fdd
Igoris Ivanovas <igoris.x@inbox.lt>
2022-01-03 17:19:36 +0200

    got screen size

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import logo from "../img/logo.png";
 
 
 const NavBar = () => {
+    const [mobileScreen , setMobileScreen] = useState<boolean>(false);
+    useEffect(() => {
+        window.innerWidth <= 770 ? setMobileScreen(true) : setMobileScreen(false);
+        return     window.addEventListener('resize', () => {
+        window.innerWidth <= 770 ? setMobileScreen(true) : setMobileScreen(false);
+        
+    })
+    })
+    
+
+
+    console.log(mobileScreen);
     return (
         <div className='navBar'>
             <a className='logo'><img src={logo} alt="logo" /></a>
-            <nav className='list active'>
+            <nav className='list'>
                 {/* image  */}
                 <ul>
                     <li className='listItem'>About</li>
@@ -16,7 +28,7 @@ const NavBar = () => {
                 </ul>
                 {/* icons  */}
             </nav>
-            <div className='hamburger '><div className='hmItem1'></div>< div className='hmItem2'></div><div className='hmItem3'></div></div>
+            <div className='hamburger'><div className='hmItem1'></div>< div className='hmItem2'></div><div className='hmItem3'></div></div>
         </div>
     )
 }
