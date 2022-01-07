e29a490322406d384b47683ed5363be0d637a85d
Igoris Ivanovas <igoris.x@inbox.lt>
2022-01-07 12:10:21 +0200

    Hero section style

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
             <h1>Igoris Ivanovas</h1>
             <h3> {'{FRONT-END WEB DEVELOPER}'}</h3>
             <p>HTML . CSS . JavaScript . React .  TypeScript</p>
-            <div><button>Projects</button> <button>Hire me</button></div>
+            <div><button className='but butOutline'>Projects</button> <button className='but'>Hire me</button></div>
         </section>
         <a href="#">Scroll down arrow</a>
         <div className='socialsLink'>link with icon</div>
