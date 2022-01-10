5b9e1859f86bbe3d076014eaadb6ec1ef0650340
Igoris Ivanovas <igoris.x@inbox.lt>
2022-01-10 11:42:20 +0200

    About layout

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,15 @@ import React from 'react'
 const About = () => {
     return (
         <section className='about'>
-                about section
+                <div className="avatar"><img src="#" alt="avater" /></div>
+                <article>
+                    <header>Hello {null}</header>
+                    <p>info</p>
+                    <p>info</p>
+                    <p>info</p>
+                <a href="#">Link ot my Linkedink</a>
+                
+                </article>
         </section>
     )
 }
