7b377ca7e973922dd32e2555638ecc9431505ff7
Igoris Ivanovas <igoris.x@inbox.lt>
2022-01-12 11:46:53 +0200

    style and bug fix

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,12 @@ const About = () => {
                 <div className="avatar"><img src={avatarImg} alt="avater" /></div>
                 <article>
                     <header>Hello <span>👋</span></header>
-                    <p>I am Igoris, a web developer based in Lithuania. I develop websites using React.js and CSS, I mostly love working on the front-end using SASS and React.</p>
+                    
+                    <p>
+                        I am Igoris, a web developer <strong>based in Lithuania</strong>.
+                        I develop websites using React.js and CSS, I mostly love working on the front-end using
+                         <strong>SASS</strong> and <strong>React</strong>.
+                     </p>
                     <p>Alsou I work with TypeScript and have knowledge of HPH, Laravel, MySQL ,Git.</p>
                 <a href="#">Link ot my Linkedink</a>
                 </article>
