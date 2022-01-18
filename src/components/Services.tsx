f33f1726c8053bfbe8ee2eeacf1591126b9a5636
Igoris Ivanovas <igoris.x@inbox.lt>
2022-01-18 11:58:50 +0200

    service style

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import Service from './Service'
 
 const Services = () => {
     return (
-        <section>
+        <section className='servicesContainer'>
              <header className='header'>
                 <h1>Services</h1>
                 <p>Excited to work with you</p>
             </header>
-
+        <ul className='servicesList'>
+        <Service/>
+        <Service/>
+        <Service/>
+        </ul>
 
         </section>
     )
