9c22ecfd697ceadd46c3f0e7f2a9787f59543022
Igoris Ivanovas <igoris.x@inbox.lt>
2022-01-19 14:30:34 +0200

    section style done

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {FaCode} from 'react-icons/fa' 
+import {FaCode, FaLaptopCode} from 'react-icons/fa' 
+import {SiFreelancer} from 'react-icons/si'
 const Service = () => {
     return (
         <div className='serviceContinter'>
-               <div className="icon"><FaCode/></div>
+               <div className="icon"><FaLaptopCode/></div>
                <h2>Some title </h2>
-               <FaCode/>
                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, assumenda?</p>
         </div>
     )
